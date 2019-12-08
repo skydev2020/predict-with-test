0c454c3c6f87a63e5563d5e93b9b740261af80a2
SkyTeam2019 <skyeteam2019@outlook.com>
2019-12-08 21:18:06 +0800

    create luis client

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace predict_with_rest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
